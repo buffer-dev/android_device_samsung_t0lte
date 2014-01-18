86591278a048e93266fe4383d00bfac339bc3379
bje0716 <byon0716@gmail.com>
2014-01-18 19:37:53 +0900

    change buffer

--- buffer.mk
+++ buffer.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/buffer/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := t0lte
@@ -8,15 +8,15 @@ PRODUCT_RELEASE_NAME := t0lte
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common buffer stuff.
+$(call inherit-product, vendor/buffer/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0lte
-PRODUCT_NAME := cm_t0lte
+PRODUCT_NAME := buffer_t0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7105
 PRODUCT_MANUFACTURER := samsung
